504dd8b7708d8ebe4144607452530b9f8fb7d401
Anthony Crane <anthonycrane@hotmail.com>
2019-05-15 14:04:02 -0700

    Collection View Added
    Collection view added. Little odds and ends of functionality added and changed. Next will be Flickr API

---
+++ Dev-pixel-city/View/PhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.swift
+//  Dev-pixel-city
+//
+//  Created by Mac User on 5/15/19.
+//  Copyright © 2019 Hammerhead96. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+}
